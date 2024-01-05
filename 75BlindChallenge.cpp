dc439d7029b6de2ed95205aae1b0a5be81bced14
smirfanshah <130116402+smirfanshah@users.noreply.github.com>
2024-01-06 00:04:14 +0500

    Part6 done

--- 75BlindChallenge.cpp
+++ 75BlindChallenge.cpp
@@ -71,3 +71,35 @@ int main5() {
 
 	return 0;
 }
+vector<int> productExceptSelf(vector<int>& nums) {
+	int n = nums.size();
+	vector<int> result(n, 1);
+	int prefix = 1;
+	int postfix = 1;
+	for (int i = 0; i < n; i++)
+	{
+		result[i] *= prefix;
+		prefix *= nums[i];
+
+		result[n-1-i] *= postfix;
+		postfix *= nums[n-1-i];
+	}
+	return result;
+}
+int main6() {
+
+	vector<int> nums = { 1, 2, 3 };
+
+	vector<int> result = productExceptSelf(nums);
+	cout << "Original array: ";
+	for (int num : nums) {
+		cout << num << " ";
+	}
+
+	cout << "\nResult array: ";
+	for (int res : result) {
+		cout << res << " ";
+	}
+
+	return 0;
+}
