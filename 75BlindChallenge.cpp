7965c8b3dcce3e0be6aa0d2ad2641c40f483c42e
smirfanshah <130116402+smirfanshah@users.noreply.github.com>
2024-01-07 22:13:30 +0500

    Task9

--- 75BlindChallenge.cpp
+++ 75BlindChallenge.cpp
@@ -198,3 +198,57 @@ int main8()
 	else
 		cout << "false";
 }
+vector<vector<int>> threeSum(vector<int>& nums) {
+
+	sort(nums.begin(), nums.end());
+
+	vector<vector<int>> result;
+
+	for (int i = 0; i < nums.size(); i++) {
+		if (i > 0 && nums[i] == nums[i - 1]) {
+			continue;
+		}
+
+		int left = i + 1;
+		int right = nums.size() - 1;
+		int target = -nums[i];
+
+		while (left < right) {
+			if (nums[left] + nums[right] == target) {
+				result.push_back({ nums[i], nums[left], nums[right] });
+
+				while (left < right && nums[left] == nums[left + 1]) {//if next element is same
+					left++;
+				}
+				while (left < right && nums[right] == nums[right - 1]) {
+					right--;
+				}
+
+				left++;
+				right--;
+			}
+			else if (nums[left] + nums[right] < target) {
+				left++;
+			}
+			else {
+				right--;
+			}
+		}
+	}
+
+	return result;
+}
+int main9()
+{
+	vector<int>nums = { -1,0,1,2,-1,-4 };
+	vector<vector<int>>ans = threeSum(nums);
+	for (const auto& row : ans)
+	{
+		for (const auto& col : row)
+		{
+			cout << col<<" ";
+		}
+		cout << endl;
+	}
+
+}
