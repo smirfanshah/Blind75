938e436763ad39573d745963e84996a52af0bbdd
smirfanshah <130116402+smirfanshah@users.noreply.github.com>
2024-01-12 21:30:01 +0500

    Task16

--- 75BlindChallenge.cpp
+++ 75BlindChallenge.cpp
@@ -468,3 +468,32 @@ int main15() {
 
     return 0;
 }
+int findMin(vector<int>& nums) {
+	int res = nums[0];
+	int l=0, r = nums.size()-1;
+	while (l <= r)
+	{
+		if (nums[l] < nums[r])
+		{
+			res = min(res, nums[l]);
+			break;
+		}
+		int mid = (l + r) / 2;
+		res = min(res,nums[mid]);
+		if (nums[mid] >= nums[l])
+		{
+			l = mid+1;
+		}
+		else
+		{
+			r = mid-1;
+		}
+	}
+	return res;
+}
+int main16()
+{
+	vector<int>nums = { 2,1};
+	int a=findMin(nums);
+	cout << "Min =" << a;
+}
