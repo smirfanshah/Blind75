9d5994a747a2c700d76d0632c605a657d2febdfd
smirfanshah <130116402+smirfanshah@users.noreply.github.com>
2024-01-18 23:04:46 +0500

    Task25

--- 75BlindChallenge.cpp
+++ 75BlindChallenge.cpp
@@ -954,3 +954,25 @@ int main24() {
 	printTree(root);
 	return 0;
 }
+int maxDepth(TreeNode* root) {
+	if (root == nullptr)
+	{
+		return 0;
+	}
+	int left = maxDepth(root->left);
+	int right = maxDepth(root->right);
+	return max(left, right) + 1;
+}
+int main25()
+{
+	TreeNode* root = new TreeNode(1);
+	root->left = new TreeNode(2);
+	root->right = new TreeNode(3);
+	root->left->left = new TreeNode(4);
+	root->left->right = new TreeNode(5);
+	root->right->left = new TreeNode(6);
+	root->right->right = new TreeNode(7);
+	int depth=maxDepth(root);
+	cout << "\nDepth=" << depth;
+	return 0;
+}
