b413ba6abb6e1688fcdd2515d4947be9b4e52a86
smirfanshah <irfanshah0422@gmail.com>
2025-06-27 10:44:14 +0500

    Gloria 2

--- CodeSignal-InterviewPrac.py
+++ CodeSignal-InterviewPrac.py
@@ -1,4 +1,4 @@
-def solution(arrayA, arrayB):
+def solution1(arrayA, arrayB):
     indexA = 0
     indexB = 0
     visited = []
@@ -19,13 +19,55 @@ def main():
     arrayB = [5, 4, 3, 2, 1]
     expected_output = [1, 4, 3, 2, 5]
     
-    result = solution(arrayA, arrayB)
+    result = solution1(arrayA, arrayB)
     print("Output:", result)
     
     if result == expected_output:
         print("Test passed!")
     else:
         print("Test failed.")
+
+def solution2(arrayA, arrayB, arrayC):
+    indexA = 0
+    indicesB = set()
+    indicesC = set()
+    maxB = 0
+    maxC = 0 
+    while True:
+        if indexA >= len(arrayA):
+            return maxB + maxC
+        
+        indexB = arrayA[indexA]
+        if indexB in indicesB or indexB >= len(arrayA):
+            return maxB + maxC
+        indicesB.add(indexB)
+            
+        indexA = arrayB[indexB]
+        maxB= max(maxB,indexA)
+
+        if indexA >= len(arrayA):
+            return maxB + maxC
+        
+        indexC = arrayA[indexA]
+        if indexC in indicesC or indexC >= len(arrayA):
+            return maxB + maxC
+        indicesC.add(indexC)
+        indexA = arrayC[indexC]
+        maxC= max(maxC,indexA)
+
+def main():
+    arrayA = [2, 1, 3, 0]
+    arrayB = [1, 3, 2, 4]
+    arrayC = [4, 2, 5, 1]
+    expected_output= 7
+    result = solution2(arrayA, arrayB, arrayC)
+    print("Output:", result)
+    
+    if result == expected_output:
+        print("Test passed!")
+    else:
+        print("Test failed.")
+
 
 # Call main
 if __name__ == "__main__":
